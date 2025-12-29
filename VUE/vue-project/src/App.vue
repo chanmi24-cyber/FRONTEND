e096d579f93054cea26e5c2d1be736eea4eb3661
kimchanmi <chanmi24@gmail.com>
2025-12-29 11:02:56 +0900

    good

---
+++ VUE/vue-project/src/App.vue
@@ -0,0 +1,10 @@
+<!--로직담당-->
+<script setup>
+  import FrontWheel from'@/components/FrontWheel.vue';
+//@사용할 경우 src기준에서 절대주소에 가까움.
+</script>
+
+<!--화면(View)담당 -->
+<template>
+   <FrontWheel></FrontWheel>
+</template>
